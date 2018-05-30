d19181f791cd4f0b0b88f6b3d423a7e08c89bb84
Brian Donovan <donovan@squareup.com>
2018-05-30 11:32:14 -0700

    Fix some minor issues with xato script.
    - Move stats variables into `main` as they are not needed outside of it.
    - Turn `valid` into a single expression.
    - Use argument spread rather than `.apply`.
    - Use a `Map` rather than a POJO for password -> count map.
    - Use deterministic ordering by sorting by count DESC, password ASC.

--- data-scripts/count_xato.js
+++ data-scripts/count_xato.js
@@ -30,9 +30,11 @@ then change into data-scripts directory and run:
 coffee count_xato.coffee --nodejs xato_file.txt ../data/passwords.txt
 \
 `;
-  let valid = process.argv.length === 5;
-  valid = valid && (process.argv[0] === 'coffee') && ['--nodejs', '-n'].includes(process.argv[2]);
-  valid = valid && (__dirname.split('/').slice(-1)[0] === 'data-scripts');
+  const valid =
+    process.argv.length === 5
+    && (process.argv[0] === 'coffee') && ['--nodejs', '-n'].includes(process.argv[2])
+    && (__dirname.split('/').slice(-1)[0] === 'data-scripts');
+
   if (!valid) {
     console.log(usage);
     process.exit(0);
@@ -45,10 +47,6 @@ const CUTOFF = 10;
 // to save memory, after every batch of size BATCH_SIZE, go through counts and delete
 // long tail of entries with only one count.
 const BATCH_SIZE = 1000000;
-
-const counts = {};       // maps pw -> count
-let skipped_lines = 0; // skipped lines in xato file -- lines w/o two tokens
-let line_count = 0;    // current number of lines processed
 
 function normalize(token) {
   return token.toLowerCase();
@@ -73,7 +71,7 @@ function should_include(password, xato_rank) {
     matching.regex_match,
     matching.date_match
     ]) {
-    matches.push.apply(matches, matcher.call(matching, password));
+    matches.push(...matcher.call(matching, password));
   }
   matches = matches.filter(match =>
     // only keep matches that span full password
@@ -90,15 +88,17 @@ function should_include(password, xato_rank) {
 }
 
 function prune(counts) {
-  for (let pw in counts) {
-    const count = counts[pw];
+  for (const [pw, count] of counts) {
     if (count === 1) {
-      delete counts[pw];
+      counts.delete(pw);
     }
   }
 }
 
 function main(xato_filename, output_filename) {
+  const counts = new Map();  // maps pw -> count
+  let skipped_lines = 0;     // skipped lines in xato file -- lines w/o two tokens
+  let line_count = 0;        // current number of lines processed
   const stream = byline.createStream(fs.createReadStream(xato_filename, {encoding: 'utf8'}));
   stream.on('readable', () => {
     let line;
@@ -115,38 +115,38 @@ function main(xato_filename, output_filename) {
       }
       let [username, password] = tokens.slice(0, 2);
       password = normalize(password);
-      if (password in counts) {
-        counts[password] += 1;
+      if (counts.has(password)) {
+        counts.set(password, counts.get(password) + 1);
       } else {
-        counts[password] = 1;
+        counts.set(password, 1);
       }
     }
   });
   stream.on('end', function() {
-    let count;
     console.log('skipped lines:', skipped_lines);
     let pairs = [];
     console.log('copying to tuples');
-    for (var pw in counts) {
-      count = counts[pw];
+    for (const [pw, count] of counts) {
       if (count > CUTOFF) {
         pairs.push([pw, count]);
       }
-      delete counts[pw];
-    } // save memory to avoid v8 1GB limit
+      // save memory to avoid v8 1GB limit
+      counts.delete(pw);
+    }
     console.log('sorting');
-    pairs.sort((p1, p2) =>
-      // sort by count. higher counts go first.
-      p2[1] - p1[1]);
-    console.log('filtering');
-    pairs = pairs.filter(function(pair, i) {
-      const rank = i + 1;
-      [pw, count] = pair;
-      return should_include(pw, rank);
+    // sort by count DESC, password ASC
+    pairs.sort(([password1, count1], [password2, count2]) => {
+      const diff = count2 - count1;
+      if (diff === 0) {
+        return password1.localeCompare(password2);
+      } else {
+        return diff;
+      }
     });
+    console.log('filtering');
+    pairs = pairs.filter(([pw, _], i) => should_include(pw, i + 1));
     const output_stream = fs.createWriteStream(output_filename, {encoding: 'utf8'});
-    for (let pair of pairs) {
-      [pw, count] = pair;
+    for (let [pw, count] of pairs) {
       output_stream.write(sprintf("%-15s %d\n", pw, count));
     }
     output_stream.end();
